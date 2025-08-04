81dbce04706ff1d4b3369367241cee7e04c5de94
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-08-04 16:57:22 +0200

    Inventory microservice impl

---
+++ inventory-service/src/main/java/com/pgs/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.pgs.microservices.inventory.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pgs.microservices.inventory.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+	boolean existsBySkuCodeAndQuantityGreaterThanEqual(String skuCode, Integer quantity);
+
+}
---
+++ inventory-service/src/main/java/com/pgs/microservices/inventory/service/InventoryService.java
@@ -0,0 +1,34 @@
+package com.pgs.microservices.inventory.service;
+
+import org.springframework.stereotype.Service;
+
+import com.pgs.microservices.inventory.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+	private final InventoryRepository inventoryRepository ;
+	
+	/**
+	 * Checks if a product with the given SKU code is currently in stock with at least the specified quantity.
+	 *
+	 * @param skuCode The unique identifier of the product.
+	 * @param quantity The minimum desired quantity to check against.
+	 * @return {@code true} if the product is in stock with the required quantity; {@code false} otherwise.
+	 */
+	public boolean isInStock(String skuCode, Integer quantity) {
+        log.debug("Checking stock for SKU: {} with requested quantity: {}", skuCode, quantity);
+		boolean inStock = this.inventoryRepository.existsBySkuCodeAndQuantityGreaterThanEqual(skuCode, quantity);
+        if (inStock) {
+            log.info("Product with SKU: {} is in stock with sufficient quantity.", skuCode);
+        } else {
+            log.warn("Product with SKU: {} is NOT in stock or does not have sufficient quantity.", skuCode);
+        }
+        return inStock;
+	}
+}
