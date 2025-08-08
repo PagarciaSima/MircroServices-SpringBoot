9801bfc13fe4c84c3e0e951e080600944257ac67
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-08-08 16:38:10 +0200

    Angular & Cors API Gateway

--- api-gateway/src/main/java/com/pgs/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/pgs/microservices/gateway/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.pgs.microservices.gateway.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -35,10 +40,21 @@ public class SecurityConfig {
 	    	    // Require authentication for any request to the application
 	    	     .anyRequest().authenticated()
 	    	)
+    		 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 	        // Configure the application as an OAuth2 Resource Server using JWT tokens
 	        .oauth2ResourceServer(oauth2 -> 
 	            oauth2.jwt(Customizer.withDefaults()) // Use default JWT decoder and validation settings
 	        )
 	        .build(); // Build and return the SecurityFilterChain bean
 	}
+	
+	 @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
