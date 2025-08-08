2b1f4f52979fa341c3f020b025d03317b46e3275
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-08-08 07:49:09 +0200

    Order service circuit breaker

--- order-service/src/main/java/com/pgs/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/pgs/microservices/order/client/InventoryClient.java
@@ -1,7 +1,12 @@
 package com.pgs.microservices.order.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 /**
  * This interface uses Spring's modern declarative HTTP client with @GetExchange,
@@ -12,7 +17,18 @@ import org.springframework.web.service.annotation.GetExchange;
  * native Spring alternative.
  */
 public interface InventoryClient {
+	
+	final static Logger LOGGER = LoggerFactory.getLogger(InventoryClient.class);
 
 	@GetExchange("/api/inventory")
+	// This name should match the app properties for resilience setup
+	@CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+	// Retry if request fails
+	@Retry(name = "inventory")
 	boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+	
+	default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+		LOGGER.info("Cannot get inventory for skucode {}, failure reason {}", code, throwable.getMessage());
+		return false;
+	}
 }
--- order-service/src/main/java/com/pgs/microservices/order/configuration/RestCLientConfig.java
+++ order-service/src/main/java/com/pgs/microservices/order/configuration/RestCLientConfig.java
@@ -1,8 +1,13 @@
 package com.pgs.microservices.order.configuration;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -32,6 +37,7 @@ public class RestCLientConfig {
 	    // Build the RestClient with the base URL of the inventory service
 	    RestClient restClient = RestClient.builder()
 	            .baseUrl(inventoryServiceUrl)  // e.g. "http://localhost:8082"
+	            .requestFactory(getClientRequestFactory()) // Config for http request timeout
 	            .build();
 
 	    // Adapt the RestClient so it can be used by Spring's HTTP service proxy factory
@@ -42,5 +48,26 @@ public class RestCLientConfig {
 
 	    // Create and return the client proxy that implements InventoryClient interface
 	    return httpServiceProxyFactory.createClient(InventoryClient.class);
+	}
+
+	/**
+	 * Creates and configures a ClientHttpRequestFactory with custom timeout settings.
+	 * 
+	 * This factory is used to create HTTP requests with specific connection and read timeouts.
+	 * It ensures that the client will wait up to 3 seconds to establish a connection 
+	 * and up to 3 seconds to read data from the server before timing out.
+	 *
+	 * @return a configured ClientHttpRequestFactory instance with timeouts set
+	 */
+	private ClientHttpRequestFactory getClientRequestFactory() {
+	    // Start with the default settings for the ClientHttpRequestFactory
+	    ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+	            // Set the maximum time to establish a connection to 3 seconds
+	            .withConnectTimeout(Duration.ofSeconds(3))
+	            // Set the maximum time to wait for reading data to 3 seconds
+	            .withReadTimeout(Duration.ofSeconds(3));
+	    
+	    // Create and return a ClientHttpRequestFactory instance using the above settings
+	    return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
 	}
 }
